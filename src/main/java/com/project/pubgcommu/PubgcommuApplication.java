1e020706d892aefd8eff57357b9c04385b318bd5
namu1714 <namu1714@naver.com>
2021-07-02 00:27:57 +0900

    프로젝트 생성, 의존성 설정 완료

---
+++ src/main/java/com/project/pubgcommu/PubgcommuApplication.java
@@ -0,0 +1,13 @@
+package com.project.pubgcommu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PubgcommuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PubgcommuApplication.class, args);
+	}
+
+}
